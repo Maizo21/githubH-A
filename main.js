b49132e9871f9410c2d8aef17a107b3b3aecc08c
Hernan Amaiz <hernanamaizp@gmail.com>
2020-11-04 17:14:47 -0300

    modificacion de menu por un menu tipo burger 1.5

--- main.js
+++ main.js
@@ -4,8 +4,27 @@ menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
+    
+    espacio.innerHTML=(` <ul>
+    <li>
+        <a class="link" href="#portafolio">Portafolio</a>
+    </li>
+    
+    <li>
+        <a class="link" href="#estudios">Estudios</a>
+    </li>
+    
+    <li>
+        <a class="link" href="#contacto">Contacto</a>
+    </li>
+</ul> `)
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
+    espacio.innerHTML='';
+    
   }
 }); 
+
+let espacio = document.getElementById('list')
+
