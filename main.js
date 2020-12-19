58e71947e65d790688593526cba3b21257177ee4
Hernan Amaiz <hernanamaizp@gmail.com>
2020-12-19 20:03:31 -0300

    Responsive

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
-    menuOpen = true;
-    
-    espacio.innerHTML=(` <ul>
-    <li>
-        <a class="link" href="#portafolio">Portafolio</a>
-    </li>
-    
-    <li>
-        <a class="link" href="#estudios">Estudios</a>
-    </li>
-    
-    <li>
-        <a class="link" href="#contacto">Contacto</a>
-    </li>
-</ul> `)
-  } else {
-    menuBtn.classList.remove('open');
-    menuOpen = false;
-    espacio.innerHTML='';
-    
-  }
-}); 
 
-let espacio = document.getElementById('list')
+const ipad = window.matchMedia('screen and (max-width: 767px');
+const menu = document.querySelector('.list');
+const burgerButton = document.querySelector('#burger-menu');
+
+
+ipad.addListener(validation)
+
+function validation(e){
+    if (e.matches){
+        burgerButton.addEventListener('click', hideShow);
+    }else{
+        burgerButton.removeEventListener('click', hideShow);
+    }
+}
+
+validation(ipad);
+
+function hideShow(){
+    if(menu.classList.contains('is-active')){
+        menu.classList.remove('is-active')
+    }else{
+        menu.classList.add('is-active')
+    };
+}
+
+function hideMenu(){
+    menu.classList.remove('is-active')
+}
+
+
+
 
